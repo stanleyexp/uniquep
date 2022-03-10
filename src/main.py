72657042073940be7d553447291b6b6140c3c4ef
tingwei628 <sbvq6p199xe@gmail.com>
2022-03-10 11:28:05 +0800

    fix ipa and correct pingyin '忒' from tei to te

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def txt_to_csv(input_file, output_file):
 
 def ping_to_ipa(input_file, output_file):
     """
-    current package: https://github.com/tsroten/dragonmapper
+    current package: https://github.com/stanleyexp/dragonmapper
     another package: https://github.com/Connum/npm-pinyin2ipa
     """
 
@@ -40,10 +40,7 @@ def ping_to_ipa(input_file, output_file):
                     new_row.append(col[0])
                     new_row.append(col[1])
                     new_row.append(col[2].strip())
-                    # ipa = transcriptions.pinyin_to_ipa(
-                    #     convert(col[2]))
-                    # ipa = transcriptions.pinyin_to_ipa(col[2])
-                    ipa = transcriptions.pinyin_to_ipa(col[2])
+                    ipa = transcriptions.pinyin_to_ipa(convert(col[2]))
                     new_row.append(ipa.strip())
                     writer.writerow(new_row)
 
@@ -64,22 +61,13 @@ def convert(col_string, has_notation=False):
     col_list = col_string.split(" ")
 
     for index, col1 in enumerate(col_list):
-        
-        if re.search(r'yo\d', col1):
-            p1 = r'yo' if has_notation else r'yo\d'
-            col_list[index] = re.sub(p1, 'yao', col1)
-        elif re.search(r'lv\d', col1):
-            p2 = r'lv' if has_notation else r'lv\d'
-            col_list[index] = re.sub(p2, 'lyu', col1)
+        # replace v with ü
+        if re.search(r'lv\d', col1):
+            p2 = r'v' if has_notation else r'v\d'
+            col_list[index] = re.sub(p2, 'ü', col1)
         elif re.search(r'nv\d', col1):
-            p3 = r'nv' if has_notation else r'nv\d'
-            col_list[index] = re.sub(p3, 'nyu', col1)
-        elif re.search(r'ei\d', col1):
-            p4 = r'ei' if has_notation else r'ei\d'
-            col_list[index] = re.sub(p4, 'e', col1)
-        elif re.search(r'dia\d', col1):
-            p5 = r'dia' if has_notation else r'dia\d'
-            col_list[index] = re.sub(p5, 'dya', col1)
+            p3 = r'v' if has_notation else r'v\d'
+            col_list[index] = re.sub(p3, 'ü', col1)
         else:
             if not has_notation:
                 col_list[index] = re.sub(r'\d$', '', col1)
@@ -87,7 +75,8 @@ def convert(col_string, has_notation=False):
     return ' '.join(col_list)
     
 def start():
-    # txt_to_csv("cidian_zhzh-kfcd-2021714.txt", "cidian_zhzh-kfcd-2021714.csv")
+    # txt_to_csv("cidian_zhzh-kfcd-2021714.txt",
+    #     "cidian_zhzh-kfcd-2021714.csv")
     # ping_to_ipa("cidian_zhzh-kfcd-2021714.csv", 
     #     "cidian_zhzh-kfcd-2021714-ipa.csv")
     ping_to_ipa("cidian_zhzh-kfcd-2021714.csv", 
